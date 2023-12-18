f1c3b8deb6ef42511345f464f03cbf172c6986bf
fardis1 <fardis@automedia.de>
2023-12-18 11:10:32 +0100

    first commit

---
+++ src/store/index.js
@@ -0,0 +1,14 @@
+import { createStore } from "vuex";
+import state from "./state";
+import mutations from "./mutations";
+import actions from "./actions";
+import getters from "./getters";
+
+const store = createStore({
+  state,
+  actions,
+  mutations,
+  getters,
+});
+
+export default store;
