2a52bde2bdc4dbd1ced68ffb3b46f4c1e1a0b9d8
fardis1 <fardis@automedia.de>
2023-12-18 13:07:39 +0100

    Activate the router for Letters

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,8 @@
 import { createStore } from "vuex";
 import state from "./state";
-import mutations from "./mutations";
-import actions from "./actions";
-import getters from "./getters";
+import * as mutations from "./mutations";
+import * as actions from "./actions";
+import * as getters from "./getters";
 
 const store = createStore({
   state,
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,2 +1,29 @@
-<template>Home</template>
-<script setup></script>
+<template>
+  <div class="flex flex-col p-8">
+    <input
+      type="text"
+      class="rounded border-2 border-gray-200 w-full"
+      placeholder="Search for meals"
+    />
+    <div class="flex justify-center gap-2 mt-2">
+      <router-link
+        :to="{ name: 'byLetter', params: { letter } }"
+        v-for="letter in letters.split('')"
+        :key="letter"
+      >
+        {{ letter }}
+      </router-link>
+    </div>
+  </div>
+</template>
+<script setup>
+import { computed, onMounted } from "vue";
+import store from "../store";
+import axiosClient from "../axiosClient";
+
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+onMounted(async () => {
+  const response = await axiosClient.get("/list.php?i=list");
+  console.log(response.data);
+});
+</script>
