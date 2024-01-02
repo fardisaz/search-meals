9e346c0e2290b9c1745a3c1cb8444614338c58c7
fardis1 <fardis@automedia.de>
2024-01-02 13:48:25 +0100

    check the url for prod

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ const isProduction = process.env.NODE_ENV === "production";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: isProduction ? "/search-meals/" : "/",
+  base: isProduction ? "/search-meals" : "/",
 });
